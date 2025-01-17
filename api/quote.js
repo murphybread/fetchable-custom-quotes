1dd2d5599d6f8946ef7879ea527c08f50c033a70
Murphybread <nargene@gmail.com>
2025-01-17 18:02:38 +0900

    feat: change path and route

--- api/quote.js
+++ api/quote.js
@@ -2,25 +2,23 @@
 import quotes from "../data.json" with { type: "json" };
 
 export default async function handler(req, res) {
-  const { pathname, searchParams } = new URL(req.url, `http://${req.headers.host}`);
+  const { searchParams } = new URL(req.url, `http://${req.headers.host}`);
+  const id = searchParams.get("id");
+  const quote = quotes.find((q) => q.id === id);
 
-  if (pathname === "/") {
-    return res.status(200).json({
-      message: "Welcome to the Quotes API!",
-      endpoints: {
-        getAllQuotes: "/api/quote",
-        getQuoteById: "/api/quote?id=<quote_id>",
-      },
-    });
-  } else if (pathname === "/api/quote") {
-    const id = searchParams.get("id");
-    const quote = quotes.find((q) => q.id === id)
-    if (id && quote ) {
-        return res.status(200).json(quote);
+  if (id !== null) {
+    // Case 1: ID 파라미터가 있는 경우
+    const quote = quotes.find((q) => q.id === id);
+    if (quote) {
+      // Case 1-1: ID에 해당하는 quote가 있는 경우
+      return res.status(200).json(quote);
     } else {
-      return res.status(404).json({ message: "Quote not found", quotes: quotes  });
+      // Case 1-2: ID에 해당하는 quote가 없는 경우
+      return res.status(404).json({ message: "Quote not found" , quote: quote});
     }
   } else {
-    return res.status(404).json({ message: "Not Proper Request" });
+    // Case 2: ID 파라미터가 없는 경우
+    return res.status(404).json({ message: "ID parameter is missing" });
   }
+  
 }
